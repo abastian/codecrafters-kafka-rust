324a7d6ffbd3a3e78576776564d43ab6d6520d3c
Arief Bastian HB <ariefbastian@gmail.com>
2025-03-13 08:17:42 +0000

    add: send correlation_id on response from request

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![allow(unused_imports)]
 use std::{
-    io::Write,
+    io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
 };
 
-use bytes::{Buf, BufMut, BytesMut};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -31,21 +31,77 @@ enum KafkaError {
     IOError(#[from] std::io::Error),
 }
 
+type CompactArray = Vec<()>;
+
+struct NullableString {
+    sz: i16,
+    data: Option<bytes::Bytes>,
+}
+
+enum RequestHeader {
+    RequestHeaderV2 {
+        request_api_key: i16,
+        request_api_version: i16,
+        correlation_id: i32,
+        client_id: NullableString,
+        tag_buffer: CompactArray,
+    },
+}
+
+struct Request {
+    message_sz: i32,
+    header: RequestHeader,
+    body: (),
+}
+
 fn handle(mut stream: TcpStream) -> Result<(), KafkaError> {
+    let mut reader = BufReader::new(&mut stream);
+    let mut read_buffer = reader.fill_buf().map_err(KafkaError::IOError)?;
+    let mut consume = 0;
+
+    // request consist of three parts,
+    // message_sz part
+    let message_sz = read_buffer.get_i32();
+    consume += 4;
+    println!("message_sz: {}", message_sz);
+
+    // header part
+    // - request_api_key
+    let request_api_key = read_buffer.get_i16();
+    consume += 2;
+    // - request_api_version
+    let request_api_version = read_buffer.get_i16();
+    consume += 2;
+    // - correlation_id
+    let correlation_id = read_buffer.get_i32();
+    consume += 4;
+    // - client_id :
+    //   + sz
+    let client_id_sz = read_buffer.get_i16();
+    consume += 2;
+    //   + data
+    let client_id_data = if client_id_sz == -1 {
+        None
+    } else {
+        let data = read_buffer.copy_to_bytes(client_id_sz as usize);
+        consume += client_id_sz as usize;
+
+        Some(data)
+    };
+    reader.consume(consume);
+
     let mut buffer = BytesMut::with_capacity(8);
 
     // message size
-    buffer.put_i32(0);
+    buffer.put_i32(4);
 
     // header
     // - correlation id
-    buffer.put_i32(7);
+    buffer.put_i32(correlation_id);
 
     let response = buffer.freeze();
-
-    while response.has_remaining() {
-        stream.write(&response).map_err(KafkaError::IOError)?;
-    }
+    stream.write_all(&response).map_err(KafkaError::IOError)?;
+    stream.flush().map_err(KafkaError::IOError)?;
 
     Ok(())
 }
