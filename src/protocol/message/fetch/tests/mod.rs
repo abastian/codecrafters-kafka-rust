6659ff8c9be6e98eb212ab95ff43d823c4759844
Arief Bastian HB <ariefbastian@gmail.com>
2025-04-04 08:01:11 +0000

    Add Fetch protocol message unit tests

--- src/protocol/message/fetch/tests/mod.rs
+++ src/protocol/message/fetch/tests/mod.rs
@@ -1,14 +1,39 @@
 use bytes::Buf;
+use protocol::message::RequestHeader;
 
 use crate::{
     metadata::{Record, RecordBatch},
     protocol::{message::ResponseHeader, Readable, ReadableVersion},
 };
 
-use super::{response::Response, *};
+use super::{request::Request, response::Response, *};
 
 #[test]
-fn test_read_fetch_multi_messages() {
+fn test_read_fetch_multi_partition_request() {
+    #[rustfmt::skip]
+    let data: Vec<u8> = vec![
+        0x00, 0x00, 0x00, 0x60, 0x00, 0x01, 0x00, 0x10, 0x4c, 0x1a, 0x89, 0x27, 0x00, 0x09, 0x6b, 0x61,
+        0x66, 0x6b, 0x61, 0x2d, 0x63, 0x6c, 0x69, 0x00, 0x00, 0x00, 0x01, 0xf4, 0x00, 0x00, 0x00, 0x01,
+        0x03, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x86, 0x02, 0x00,
+        0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x10, 0x00, 0x00, 0x00,
+        0x00, 0x01, 0x01, 0x00,
+    ];
+
+    let data_ref: &[u8] = data.as_ref();
+    let mut read_buffer = data_ref;
+
+    let sz_request = i32::read(&mut read_buffer);
+    assert_eq!(sz_request as usize, read_buffer.remaining());
+    let header = RequestHeader::read_result(&mut read_buffer).unwrap();
+    assert_eq!(super::API_KEY, header.request_api_key());
+    assert_eq!(16, header.request_api_version());
+    let _req = Request::read_version(&mut read_buffer, header.request_api_version()).unwrap();
+}
+
+#[test]
+fn test_read_fetch_multi_partition_response() {
     #[rustfmt::skip]
     let data: Vec<u8> = vec![
         0x00, 0x00, 0x01, 0x15, 0x76, 0x3b, 0x25, 0x54, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -34,25 +59,71 @@ fn test_read_fetch_multi_messages() {
     let data_ref: &[u8] = data.as_ref();
     let mut read_buffer = data_ref;
 
-    let _sz_request = i32::read(&mut read_buffer);
+    let sz_request = i32::read(&mut read_buffer);
+    assert_eq!(sz_request as usize, read_buffer.remaining());
     let _header = ResponseHeader::read_version(&mut read_buffer, 1).unwrap();
     let resp = Response::read_version(&mut read_buffer, 16).unwrap();
-    for response in resp.responses {
-        for partition in response.partitions() {
-            let Some(mut record) = partition.records().cloned() else {
-                continue;
-            };
-
-            while record.has_remaining() {
-                let rb = RecordBatch::read_result(&mut record).unwrap();
-                for record in rb.records() {
-                    let Record::Value(v) = record else {
-                        continue;
-                    };
-                    let value = std::str::from_utf8(v.value().as_ref()).unwrap();
-                    println!("value: {}", value);
-                }
-            }
+    assert_eq!(1, resp.responses().len(), "responses len");
+    assert_eq!(
+        2,
+        resp.responses[0].partitions().len(),
+        "partitions len of responses[0]"
+    );
+    {
+        let mut records = resp.responses()[0].partitions()[0]
+            .records()
+            .unwrap()
+            .clone();
+        let mut values = Vec::with_capacity(2);
+        while records.has_remaining() {
+            let rb = RecordBatch::read_result(&mut records).unwrap();
+            values.push(rb);
         }
-    }
+
+        assert_eq!(
+            2,
+            values.len(),
+            "record-batches len of responses[0] partitions[0]"
+        );
+        assert_eq!(
+            1,
+            values[0].records().len(),
+            "records len of responses[0] partitions[0] record-batches[0]"
+        );
+        if let Record::Value(record_value) = &values[0].records()[0] {
+            assert_eq!(
+                "Hello Kafka!",
+                std::str::from_utf8(record_value.value().as_ref()).unwrap(),
+                "value[0] of responses[0] partitions[0] record-batches[0] records[0]"
+            )
+        } else {
+            assert!(
+                false,
+                "responses[0] partitions[0] record-batches[0] is not a value-record"
+            );
+        };
+
+        assert_eq!(
+            1,
+            values[1].records().len(),
+            "records len of responses[0] partitions[0] record-batches[1]"
+        );
+        if let Record::Value(record_value) = &values[1].records()[0] {
+            assert_eq!(
+                "Hello CodeCrafters!",
+                std::str::from_utf8(record_value.value().as_ref()).unwrap(),
+                "value[0] of responses[0] partitions[0] record-batches[1] records[0]"
+            )
+        } else {
+            assert!(
+                false,
+                "responses[0] partitions[0] record-batches[1] is not a value-record"
+            );
+        };
+    };
+    assert_eq!(
+        None,
+        resp.responses()[0].partitions()[1].records(),
+        "responses[0] partitions[0] has empty records"
+    );
 }
